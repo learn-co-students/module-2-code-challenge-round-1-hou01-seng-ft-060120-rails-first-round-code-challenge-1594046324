872e36f57a7e0d080513a85ccf95e744179940c2
franciscosierra1915 <fransierra.s.15@gmail.com>
2020-07-06 13:51:51 -0400

    submission for deadline

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,38 @@
 class HeroinesController < ApplicationController
+
   def index
     @heroines = Heroine.all
   end
+
+  def show 
+    @heroine = Heroine.find(params[:id])
+    # @power = Power.find(params[:id])
+  end
+
+  def new 
+    @heroine = Heroine.new
+  end
+  
+  def create 
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.valid?
+      @heroine.save
+      redirect_to heroine_path(@heroine)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+  
+  def update
+  end
+
+  private 
+  
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
 end
