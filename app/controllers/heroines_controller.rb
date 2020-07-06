64bcefe80644ed7783afc9dae7764304c938e7d7
reynaldoayala062 <reynaldo-ayala@hotmail.com>
2020-07-06 13:02:44 -0500

    reynaldo-ayala

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,29 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @hero = Heroine.find(params[:id])
+  end
+
+  def new 
+    @hero = Heroine.new
+  end 
+
+  def create
+    @hero = Heroine.create(power_params)
+
+    if @hero.save
+      redirect_to heroine_path(@hero)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def power_params
+    params.require(:heroine).permit(:name, :super_name, power_ids_attributes: [:id, power_ids: []])
+  end
+
 end
